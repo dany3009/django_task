dbbdf61b96079a7dc32b27f6eef44ab7fd0a0bd7
dany3009 <dany3009@rambler.ru>
2013-04-20 22:09:30 +0300

    Second commit

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-from task.views import group_list
+from task.views import *
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', group_list),
+    url(r'^login/$', login),
+    url(r'^group/(?P<name>\w+\W+\d+)', student_list),
     url(r'^admin/', include(admin.site.urls)),
 )
--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
 from django.template import loader, Context
 from django.http import HttpResponse
 from task.models import Student, Group
+from django.contrib import auth
 
 def group_list(request):
     groups = Group.objects.all()
@@ -8,3 +9,29 @@ def group_list(request):
     templ = loader.get_template('home.html')
     cont = Context({ 'groups' : groups, 'students' : students})
     return HttpResponse(templ.render(cont))
+
+def student_list(request, name):
+    students = Student.objects.all()
+    list =[]
+    
+    for student in students:
+        if (name == student.group):
+            list.append(student)
+            
+    templ = loader.get_template('students.html')
+    cont = Context({ 'list' : list })
+    return HttpResponse(templ.render(cont))
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username = username, password = password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponse('Invalid Login')
+    else:
+        return HttpResponse('Invalid Login')
