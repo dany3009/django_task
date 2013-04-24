059651d6d9887ebda79d35c917a38d8ccaa6ab15
Philipp Bliznuk <philipp.bliznuk@gmail.com>
2013-04-24 14:28:37 +0300

    Initial commit

--- task/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'group')
-    
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'warden')
