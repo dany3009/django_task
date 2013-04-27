2ba8f5a88f02358b52a1ec434ee9bea0a6f07a72
Philipp Bliznuk <philipp.bliznuk@gmail.com>
2013-04-27 23:22:00 +0300

    seven commit

---
+++ task/tests.py
@@ -0,0 +1,12 @@
+from django.test.client import Client
+from task.models import *
+import unittest
+
+class TeastAddCase(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+    def add(self):
+        self.client.login(username = 'admin', password = 'admin')
+        self.client.post('/admin/task/group/add/', { 'name' : 'TK-32', 'warden' : '3' })
+        self.client.post('/admin/task/student/add/', { 'name' : 'Bill William Klinthon', 'birth_date' : '1936-03-18', 'icket_number' : '7534', 'group' : '3' })
